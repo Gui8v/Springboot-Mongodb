d4d6e6c0779d60e60d6cb457794f65b76a06c8df
Gui8v <gui8v.rocha8@hotmail.com>
2022-11-08 16:48:36 -0300

    User insert method

--- src/main/java/com/example/springmongo/services/UserService.java
+++ src/main/java/com/example/springmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.springmongo.domain.User;
+import com.example.springmongo.dto.UserDTO;
 import com.example.springmongo.repositories.UserRepository;
 import com.example.springmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
