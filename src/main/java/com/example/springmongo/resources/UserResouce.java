dca3ea183c4e9c7f26974c68908e788e28763b45
Gui8v <gui8v.rocha8@hotmail.com>
2022-11-09 18:51:48 -0300

    User update method

--- src/main/java/com/example/springmongo/resources/UserResouce.java
+++ src/main/java/com/example/springmongo/resources/UserResouce.java
@@ -45,6 +45,14 @@ public class UserResouce {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<User> update(@RequestBody UserDTO userDTO, @PathVariable String id){
+		User user = service.fromDTO(userDTO);
+		user.setId(id);
+		user = service.update(user);
+		return ResponseEntity.noContent().build();
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
