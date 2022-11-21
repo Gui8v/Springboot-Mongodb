7d4b090481564da5c02c9d3c2838d03bef7d55a4
Gui8v <gui8v.rocha8@hotmail.com>
2022-11-21 14:45:16 -0300

    Comments to Post

--- src/main/java/com/example/springmongo/configuration/Instantiation.java
+++ src/main/java/com/example/springmongo/configuration/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.springmongo.domain.Post;
 import com.example.springmongo.domain.User;
 import com.example.springmongo.dto.AuthorDTO;
+import com.example.springmongo.dto.CommentDTO;
 import com.example.springmongo.repositories.PostRepository;
 import com.example.springmongo.repositories.UserRepository;
 
@@ -39,7 +40,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post p1 = new Post(null, sdf.parse("20/12/2022"), "Viagem !", "Viagem para São Paulo", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("27/12/2022"), "Boa Noite !", ".-.", new AuthorDTO(maria));
-
+		
+		CommentDTO c1 = new CommentDTO("Boa Viagem !", sdf.parse("21/12/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/12/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia", sdf.parse("23/12/2022"), new AuthorDTO(alex));
+		
+		p1.getComments().addAll(Arrays.asList(c1, c2));
+		p2.getComments().addAll(Arrays.asList(c3));
+		
 		PostRepository.saveAll(Arrays.asList(p1, p2));
 		
 		maria.getPosts().addAll(Arrays.asList(p1, p2));
--- src/main/java/com/example/springmongo/domain/Post.java
+++ src/main/java/com/example/springmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.example.springmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.springmongo.dto.AuthorDTO;
+import com.example.springmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,7 +21,10 @@ private static final long serialVersionUID = 1L;
 	private Date date;
 	private String title;
 	private String body;
+	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public  Post() {
 		
@@ -72,6 +78,15 @@ private static final long serialVersionUID = 1L;
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -89,7 +104,5 @@ private static final long serialVersionUID = 1L;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
+
 }
